44d1c0b02204985ab855319686a632dde946c089
thewickedgoat <noahbock@live.dk>
2017-04-06 10:05:35 +0100

    folders and some minor things added

---
+++ KantineApp/KantineApp/Interface/IData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KantineApp.Interface
+{
+    public interface IData<T>
+    {
+        void Create();
+        T Read(int id);
+        List<T> ReadAll();
+        T Update(T t);
+        void Delete(int id);
+    }
+}
